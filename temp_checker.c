579d5d52ac02fc8f4f3c82f6748f7bba3881bf5b
zabe <zmhyxd@gmail.com>
2025-12-26 12:57:54 +0300

    Enhance main function with system pause
    Added system pause before exiting the program.

--- temp_checker.c
+++ temp_checker.c
@@ -1,40 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-double convert_to_fahrenheit(double x) {
+double convert_to_fahrenheit(double x)
+{
     return (x * 1.8) + 32;
 }
 
-double convert_to_celsius(double x) {
+double convert_to_celsius(double x)
+{
     return (x - 32) / 1.8;
 }
 
-void run_code() {
+void run_code()
+{
     double temp, result;
     char unit;
 
     printf("Choose unit (F,C):\n");
     scanf(" %c", &unit);
 
-    if (unit == 'F' || unit == 'f') {
+    if (unit == 'F' || unit == 'f')
+    {
         printf("Enter temperature in Fahrenheit:\n");
         scanf("%lf", &temp);
 
         result = convert_to_celsius(temp);
         printf("Result: %.2lfC\n", result);
-
-    } else if (unit == 'C' || unit == 'c') {
+    }
+    else if (unit == 'C' || unit == 'c')
+    {
         printf("Enter temperature in Celsius:\n");
         scanf("%lf", &temp);
 
         result = convert_to_fahrenheit(temp);
         printf("Result: %.2lfF\n", result);
-
-    } else {
+    }
+    else
+    {
         printf("Invalid unit!\n");
     }
 }
 
-int main() {
+int main()
+{
     run_code();
+
+    system("pause");
+
     return 0;
 }
